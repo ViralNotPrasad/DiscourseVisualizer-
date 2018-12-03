42703cad0036b2ada5c9545df01834171b618128
sahasradude <dhruvas@vt.edu>
2018-12-03 00:02:36 -0500

    time used monitoring start

--- flask/hello.py
+++ flask/hello.py
@@ -1,32 +1,105 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 import pickle
-app = Flask(__name__)
+import time
 
+app = Flask(__name__ , static_url_path="", static_folder="static")
+datadict = {}
+timelist = []
+last_time = 0
 
-@app.route("/")
 @app.route("/home")
-def home(name):
+def home():
+    global timelist
+    global last_time
     titles = []
     bodies = []
     comments = []
-    topicset = set(["immigration", "abortion", "shooting", "education", "android"])
-    with open("../finaldict.pickle", "rb") as f:
-        datadict = pickle.load(f)
+    labels = []
+    topics = []
 
     for topic in topicset:
-        for idx in datadict["topics"][topic][:5]:
-            titles.append(datadict["titles"][idx])
-            bodies.append(datadict["bodies"][idx])
-            comments.append(datadict["comments"][idx][1][0])
+        i = 0
+        lcount = 0
+        for idx in datadict["topics"][topic]:
+            label = datadict["labels"][topic][i]
+            i+=1
+            body = datadict["bodies"][idx]
+            if label > 0 and label <= 5 and len(body) < 2000:
+                titles.append(datadict["titles"][idx])
+                bodies.append(body)
+                comments.append(datadict["comments"][idx][1][0])
+                labels.append(label)
+                topics.append(topic)
 
+                lcount+=1
+
+            if lcount == 5:
+                break;
 
     return render_template(
-        'feed.html', titles=titles, bodies=bodies, comments=comments)
+        'feed.html', l=len(titles), labels=labels, topics=topics, titles=titles, bodies=bodies, comments=comments)
+
+@app.route("/", methods=['POST', 'GET'])
+def root():
+    return render_template("form.html")
+
+@app.route("/home/click/<string:topic>/<int:label>", methods=['POST, GET'])
+def btnclk():
+    global last_time
+    global timelist
+    now_time = time.time()
+    delta = now_time - last_time
+    last_time = now_time
+    return redirect(url_for('home'))
 
 
-@app.route("/similar/<string:topic>/<int:lean>")
-def similar(topic, lean):
-    return (topic+str(lean))
+
+
+@app.route('/form', methods=['POST', 'GET'])
+def form():
+    global last_time
+    if request.method == 'POST':
+        result = request.form
+        print(result)
+    last_time = time.time()
+    return redirect(url_for("home"))
+
+@app.route("/similar/<string:topic>/<int:label>/<int:type>")
+def similar(topic, type, label):
+    global timelist
+    global last_time
+    now_time = time.time()
+    delta = now_time - last_time
+    last_time = now_time
+    timelist.append((topic, label, delta))
+    titles = []
+    bodies = []
+    comments = []
+    labels = []
+    i = 0
+    lcount = 0
+    label = int(label)
+    print(label)
+    if type == 1:  #if data is a comment
+        label = abs(6 - label)
+    print(label)
+    for idx in datadict["topics"][topic]:
+        label_new = int(datadict["labels"][topic][i])
+        body = datadict["bodies"][idx]
+        i+=1
+        print("label_new:", label_new)
+
+        if label_new == label and len(body) <= 2000:
+            titles.append(datadict["titles"][idx])
+            bodies.append(body)
+            comments.append(datadict["comments"][idx][1][0])
+            labels.append(label_new)
+            lcount += 1
+            # print("lcount= ",lcount)
+
+        if lcount == 5:
+            break;
+    return render_template('similar.html', titlen=len(titles), titles=titles, bodies=bodies, topic=topic)
 
 @app.route("/shutdown")
 def shutdown():
@@ -39,5 +112,16 @@ def shutdown_server():
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
 
-if __name__ == "__main__":
+def main():
+    global datadict
+    global topicset
+    with open("../finaldict.pickle", "rb") as f:
+        datadict = pickle.load(f)
+    with open("../labelled2.pickle","rb") as f:
+        datadict["labels"] = pickle.load(f)
+    topicset = set(["immigration", "abortion", "shooting", "education", "android"])
+
     app.run()
+
+if __name__ == "__main__":
+    main()
