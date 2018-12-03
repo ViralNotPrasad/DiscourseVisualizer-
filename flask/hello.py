0aa8a56a12654eb67f277c779114fce7f0195994
sahasradude <dhruvas@vt.edu>
2018-12-03 14:18:51 -0500

    corrections for time logging code

--- flask/hello.py
+++ flask/hello.py
@@ -6,7 +6,9 @@ app = Flask(__name__ , static_url_path="", static_folder="static")
 datadict = {}
 timelist = []
 last_time = 0
-
+current_topic_time = None
+current_label_time = None
+current_tot_post_len = None
 @app.route("/home")
 def home():
     titles = []
@@ -33,7 +35,7 @@ def home():
 
             if lcount == 5:
                 break;
-    timecheck("set")
+    timecheck("delta", current_topic_time, current_label_time, current_tot_post_len)
     return render_template(
         'feed.html', l=len(titles), labels=labels, topics=topics, titles=titles, bodies=bodies, comments=comments)
 
@@ -43,20 +45,26 @@ def root():
 
 @app.route("/home/click/<string:topic>/<int:label>")
 def btnclk(topic, label):
-    timecheck("delta", topic, label)
     return redirect(url_for('home'))
 
-def timecheck(action, topic=None, label=None):
+def timecheck(action, topic=None, label=None, postlen=None):
     global timelist
     global last_time
+    global current_topic_time
+    global current_label_time
+    global current_tot_post_len
     if action == "set":
         last_time = time.time()
+        current_topic_time = topic
+        current_label_time = label
+        current_tot_post_len = postlen
         return
     elif action == "delta":
         now_time = time.time()
         delta = now_time - last_time
         last_time = now_time
-    timelist.append((topic, label, delta))
+
+    timelist.append((topic, label, delta, postlen))
     print (timelist)
     return
 
@@ -66,12 +74,12 @@ def form():
     if request.method == 'POST':
         result = request.form
         print(result)
-    timecheck("set")
+        with open("form_result.pickle", "wb") as f:
+            pickle.dump(result, f)
     return redirect(url_for("home"))
 
 @app.route("/similar/<string:topic>/<int:label>/<int:type>")
 def similar(topic, type, label):
-    timecheck("delta", topic, label)
     titles = []
     bodies = []
     comments = []
@@ -79,6 +87,7 @@ def similar(topic, type, label):
     i = 0
     lcount = 0
     label = int(label)
+    bodies_len = 0
     if type == 1:  #if data is a comment
         label = abs(6 - label)
     for idx in datadict["topics"][topic]:
@@ -91,11 +100,14 @@ def similar(topic, type, label):
             bodies.append(body)
             comments.append(datadict["comments"][idx][1][0])
             labels.append(label_new)
+            bodies_len += len(body)
             lcount += 1
             # print("lcount= ",lcount)
 
         if lcount == 5:
             break;
+
+    timecheck("set", topic, label, bodies_len) # start the clock when user goes to similar right before rendering the page
     return render_template('similar.html', titlen=len(titles), titles=titles, bodies=bodies, topic=topic)
 
 @app.route("/shutdown")
